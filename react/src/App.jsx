4e2e3e02357da3a6182d150e17723fb50f8fd591
Manny Goat <alice@webpage.com>
2025-07-09 16:23:26 -0400

    added basic react components

---
+++ react/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Welcome from './components/Welcome'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Welcome />
+      <Footer />
+    </>
+  );
+};
+
+export default App
---
+++ react/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Footer = () => {
+    return(
+        <>
+        
+        </>
+    );
+};
+export default Footer;
